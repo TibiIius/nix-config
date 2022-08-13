903e838395bc05f8866c7ff0d14f7d0f9e10dcf8
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-08-13 20:58:00 +0200

    [home-manager.nix] add zsh plugin for nix-shell

--- home-manager.nix
+++ home-manager.nix
@@ -20,7 +20,7 @@
           enableAutosuggestions = true;
           enableCompletion = true;
           enableSyntaxHighlighting = true;
-          initExtra = "~/.dotfiles/home/.zshrc";
+          initExtra = "zsh ~/.dotfiles/home/.zshrc";
           oh-my-zsh = {
             enable = true;
           };
@@ -34,6 +34,16 @@
               name = "powerlevel10k-config";
               src = /home/tim/.dotfiles/home;
               file = ".p10k.zsh";
+            }
+            {
+              name = "zsh-nix-shell";
+              file = "nix-shell.plugin.zsh";
+              src = pkgs.fetchFromGitHub {
+                owner = "chisui";
+                repo = "zsh-nix-shell";
+                rev = "v0.5.0";
+                sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+              };
             }
           ];
         };
