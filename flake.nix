c76c33a5695963575624f05789b4f849a68ae22f
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-08-28 19:19:59 +0200

    [flake.nix] add hyprland

--- flake.nix
+++ flake.nix
@@ -16,15 +16,22 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, hyprland, ... }:
   {
     nixosConfigurations = {
       nixos-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           home-manager.nixosModules.home-manager
+          hyprland.nixosModules.default 
+          { programs.hyprland.enable = true; }
           ./configuration.nix 
           ./hosts/nixos-laptop/hardware-configuration.nix
         ];
