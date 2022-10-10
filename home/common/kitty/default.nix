d0cb5c2caa0f7ef67cec72429789d14fd4506aa9
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-10-10 03:49:05 +0200

    [home/common/kitty] add themes

--- home/common/kitty/default.nix
+++ home/common/kitty/default.nix
@@ -3,4 +3,5 @@
 {
   # Just use yaml config file as we don't want to manage GUI applications with home-manager if possible
   xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
+  xdg.configFile."kitty/themes".source = ./themes;
 }
