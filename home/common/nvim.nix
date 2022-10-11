2095787e277727a210f1044d3ccfe7465016e5df
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-10-11 11:20:14 +0200

    [home/common/nvim] fix keybinds

--- home/common/nvim.nix
+++ home/common/nvim.nix
@@ -118,16 +118,16 @@
       " Use tab for trigger completion with characters ahead and navigate.
       " Use command ':verbose imap <tab>' to make sure tab is not mapped by other plugin.
       inoremap <silent><expr> <TAB>
-            \ pumvisible() ? "\<C-n>" :
-            \ <SID>check_back_space() ? "\<TAB>" :
+            \ coc#pum#visible() ? coc#pum#next(1) :
+            \ CheckBackspace() ? "\<TAB>" :
             \ coc#refresh()
-      inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+      inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
       
       " Enter -> accept snippet
-      inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm() : 
+      inoremap <silent><expr> <cr> coc#pum#visible() ? coc#pum#confirm() : 
                                                \"\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
       
-      function! s:check_back_space() abort
+      function! CheckBackspace() abort
         let col = col('.') - 1
         return !col || getline('.')[col - 1]  =~# '\s'
       endfunction
