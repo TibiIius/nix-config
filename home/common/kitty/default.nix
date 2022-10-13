4522e5a111c5c51a5052be66d6c0a3e923958630
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-10-13 17:36:39 +0200

    [home/common/kitty] fetch themes from GitHub

--- home/common/kitty/default.nix
+++ home/common/kitty/default.nix
@@ -3,5 +3,12 @@
 {
   # Just use yaml config file as we don't want to manage GUI applications with home-manager if possible
   xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
-  xdg.configFile."kitty/themes".source = ./themes;
+  xdg.configFile."kitty/themes-custom".source = ./themes;
+  xdg.configFile."kitty/themes-github".source = pkgs.fetchFromGitHub
+    {
+      owner = "kovidgoyal";
+      repo = "kitty-themes";
+      rev = "master";
+      sha256 = "sha256-YZxt59gZ7YaEyEXinT8eU3oLB0Cmd2+lu1GZvGZSAfY=";
+    } + "/themes";
 }
