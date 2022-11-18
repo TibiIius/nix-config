ae7f477a0ad6af3b22a17aedf35d1086e9b05125
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-18 18:57:54 +0100

    [home/common/kitty] only download themes dir

--- home/common/kitty/default.nix
+++ home/common/kitty/default.nix
@@ -10,5 +10,6 @@
       repo = "kitty-themes";
       rev = "b1abdd54ba655ef34f75a568d78625981bf1722c";
       sha256 = "sha256-RcDmZ1fbNX18+X3xCqqdRbD+XYPsgNte1IXUNt6CxIA=";
-    } + "/themes";
+      sparseCheckout = "themes";
+    };
 }
