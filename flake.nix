e5722a04ce12a92bc724b0e20d5adee7b92335ad
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-12-01 01:24:37 +0100

    Revert "[flake.nix] change repo for webkitgtk patch"
    This reverts commit 523a62d8c5ed0d22d4675c75ea13b3a52bcb1ffe.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs-staging-next.url = "github:NixOS/nixpkgs/staging-next";
+    nixpkgs-jtojnar.url = "github:jtojnar/nixpkgs/webkitgtk-sandbox-fix";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -32,7 +32,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, newmpkg, flake-utils, tibiiius-pkgs, nixpkgs-staging-next, ... }:
+  outputs = { self, nixpkgs, home-manager, newmpkg, flake-utils, tibiiius-pkgs, nixpkgs-jtojnar, ... }:
     let
       inherit (flake-utils.lib) eachSystem system;
     in
@@ -49,7 +49,7 @@
               (self: super: {
                 newm = newmpkg.packages.${system}.newm;
                 # libadwaita = tibiiius-pkgs.packages.${system}.libadwaita-without-adwaita;
-                webkitgtk = nixpkgs-staging-next.legacyPackages.${system}.webkitgtk;
+                webkitgtk = nixpkgs-jtojnar.legacyPackages.${system}.webkitgtk;
               })
             ];
           };
