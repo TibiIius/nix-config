7480871659b7660bb8f955c3a15bc720d411df1f
TimFB <56914732+TibiIius@users.noreply.github.com>
2023-01-30 17:26:18 +0100

    [home/common/zsh] add distrobox handler

--- home/common/zsh/default.nix
+++ home/common/zsh/default.nix
@@ -65,6 +65,21 @@
 
       # Zoxide
       eval "$(zoxide init zsh)"
+
+      # Distrobox host communication
+      command_not_found_handle() {
+      # don't run if not in a container
+        if [ ! -e /run/.containerenv ] && [ ! -e /.dockerenv ]; then
+          exit 127
+        fi
+        
+        distrobox-host-exec "$@"
+      }
+      if [ -n "$ZSH_VERSION" ]; then
+        command_not_found_handler() {
+          command_not_found_handle "$@"
+       }
+      fi
     '';
     oh-my-zsh = {
       enable = true;
